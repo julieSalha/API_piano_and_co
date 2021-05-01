a81fc494547419ce57e945168ff217ebfb87f3a6
julieSalha <julie.salha@gmail.com>
2021-05-01 19:45:42 +0200

    Added a Procfile.

--- services/mongo.class.js
+++ services/mongo.class.js
@@ -11,12 +11,12 @@ Define class
     class MONGOClass { 
         constructor(){
             // Set MongoDB url
-            this.mongoUrl = process.env.MONGO_URL;
+            this.mongoUrl = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/api-piano';
         };
 
         connectDb(){
             return new Promise( (resolve, reject) => {
-                mongoose.connect(this.mongoUrl || 'mongodb://127.0.0.1:27017/api-piano', { useNewUrlParser: true, useUnifiedTopology: true })
+                mongoose.connect(this.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
                 .then(resolve('connected to mongodb..'))
                 .catch( dbErr => reject(`MongoDB not connected`, dbErr) )
             });
